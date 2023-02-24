e6c1bbda3454f02912b63d2188eaea37ab3f920f
IvanSanchezFernandez <31513069+IvanSanchezFernandez@users.noreply.github.com>
2023-02-23 22:50:55 -0500

    update

---
+++ Highercortical_main.js
@@ -0,0 +1,227 @@
+
+
+function checkQ1() {
+
+      if (document.question1.question.value == "d") {
+            document.getElementById("after_submitQ1").style.visibility = "visible";
+            document.getElementById("answerQ1").innerHTML = "Correct!!<br><br>Balint syndrome is characterized by ocular ataxia (inability to reach under visual guidance), simultanagnosia (inability to see the whole picture while still being able to see individual elements once at a time), and oculomotor apraxia (inaccurate saccades when shifting focus from one visual target to another).<br><br>The anatomical localization for this syndrome is <a href='https://jnnp.bmj.com/content/jnnp/72/2/162.full.pdf' target='_blank'>in the bilateral parietal regions near the temporoparietooccipital junction</a>.";
+            document.getElementById("after_submitQ1").style.background = "lightgreen";
+            document.getElementById("afterQ1_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ1").style.visibility = "visible";
+            document.getElementById("answerQ1").innerHTML = "No.";
+            document.getElementById("after_submitQ1").style.background = "red";
+            document.getElementById("afterQ1_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+function checkQ2() {
+
+      if (document.question2.question.value == "g") {
+            document.getElementById("after_submitQ2").style.visibility = "visible";
+            document.getElementById("answerQ2").innerHTML = "Exactly!!<br><br>This is Capgras delusion. In Capgras syndrome, familiar people are considered strangers (imposters).<br><br>This is a psychiatric condition. <a href='https://www.youtube.com/watch?v=dqBGzkz1oDU' target='_blank'>See this full story for a potential pathophysiological explanation</a>.";
+            document.getElementById("after_submitQ2").style.background = "lightgreen";
+            document.getElementById("afterQ2_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ2").style.visibility = "visible";
+            document.getElementById("answerQ2").innerHTML = "No.";
+            document.getElementById("after_submitQ2").style.background = "red";
+            document.getElementById("afterQ2_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+function checkQ3() {
+
+      if (document.question3.question.value == "a") {
+            document.getElementById("after_submitQ3").style.visibility = "visible";
+            document.getElementById("answerQ3").innerHTML = "Correct!! <br><br>Prosopagnosia is characterized by impairment in face perception in a patient with intact visual acuity and intact visual fields. Individuals with prosopagnosia lack the ability to perceive the unique features that distinguish an individual face from others. When looking in a mirror, they do not recognize their face as their own.<br><br>This syndrome localizes to the <a href='https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Constudproc_-_inferior_view.png/250px-Constudproc_-_inferior_view.png' target='_blank'>face area</a> in the bilateral occipitotemporal <a href='https://upload.wikimedia.org/wikipedia/commons/7/7e/Gray727_fusiform_gyrus.png' target='_blank'>fusiform</a> <a href='https://www.kenhub.com/thumbor/8C1GPCq2HcN2Z579GBuYmHhMP3U=/fit-in/413x413/filters:fill(FFFFFF,true):watermark(/images/watermark_only_sm.png,0,0,0):watermark(/images/logo_url_sm.png,-10,-10,0):format(jpeg)/images/anatomy_term/occipitotemporal-gyri/fBagn1XsnsNFsZrDHC1KFQ_Occipitotemporal_gyrus.png' target='_blank'>gyrus</a> (although unilateral lesions, most often right, can cause mild prosopagnosia).";
+            document.getElementById("after_submitQ3").style.background = "lightgreen";
+            document.getElementById("afterQ3_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ3").style.visibility = "visible";
+            document.getElementById("answerQ3").innerHTML = "No";
+            document.getElementById("after_submitQ3").style.background = "red";
+            document.getElementById("afterQ3_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+function checkQ4() {
+
+      if (document.question4.question.value == "e") {
+            document.getElementById("after_submitQ4").style.visibility = "visible";
+            document.getElementById("answerQ4").innerHTML = "Yes!! <br><br>Riddoch syndrome or statokinetic disociation occurs when the patient is able to detect motion in an otherwise blind visual field (which can be the full visual fields or parts of it). That is, the presence of an object is perceived only if it is moving and, although the object is perceived, color or shape remain unclear.<br><br>Riddoch syndrome is caused by either incomplete lesions to the visual pathways which still allow to perceive rough information on direction and speed or through <a href='https://www.researchgate.net/publication/49847078/figure/fig1/AS:282655452090374@1444401719127/Visual-pathways-in-the-brain-LGNlateral-geniculate-nucleus-PUpulvinar-nucleus.png' target='_blank'>non-geniculate visual pathways (superior colliculus-->pulvinar nucleus of the thalamus-->parietooccipital region)</a>.<br><br>See the patient's full videos <a href='https://www.dailymail.co.uk/video/health/video-1707124/Video-Patient-Riddoch-syndrome-ability-moving-objects.html' target='_blank'>here</a>.";
+            document.getElementById("after_submitQ4").style.background = "lightgreen";
+            document.getElementById("afterQ4_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ4").style.visibility = "visible";
+            document.getElementById("answerQ4").innerHTML = "No";
+            document.getElementById("after_submitQ4").style.background = "red";
+            document.getElementById("afterQ4_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+function checkQ5() {
+
+      if (document.question5.question.value == "h") {
+            document.getElementById("after_submitQ5").style.visibility = "visible";
+            document.getElementById("answerQ5").innerHTML = "Correct!! <br><br>In apperceptive visual agnosia elementary aspects of the visual information (colors, basic shapes) are properly recognized, but the ability to integrate these aspects into the full object is lost. It is similar to prosopagnosia, but for objects rather than for faces.<br><br>The lesional area in visual agnosia is the <a href='https://www.perkins.org/wp-content/uploads/1970/01/screen_shot_2021-04-30_at_2.20.44_pm.png' target='_blank'>V2 visual cortex that further processes the information after the initial processing by the V1 visual cortex</a>.";
+            document.getElementById("after_submitQ5").style.background = "lightgreen";
+            document.getElementById("afterQ5_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ5").style.visibility = "visible";
+            document.getElementById("answerQ5").innerHTML = "No";
+            document.getElementById("after_submitQ5").style.background = "red";
+            document.getElementById("afterQ5_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+function checkQ6() {
+
+      if (document.question6.question.value == "g") {
+            document.getElementById("after_submitQ6").style.visibility = "visible";
+            document.getElementById("answerQ6").innerHTML = "That is right!! <br><br>In Cotard delusion (walking corpse syndrome) the patient believes that he or she is dead and/or believes that the body or parts of the body are rotting or are missing.<br><br>It happens in patients with severe structural brain regions and psychiatric disorders, especially schizophrenia. This is a <a href='https://www.youtube.com/watch?v=H5vnn08TGbk' target='_blank'>general explanation</a>.";
+            document.getElementById("after_submitQ6").style.background = "lightgreen";
+            document.getElementById("afterQ6_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ6").style.visibility = "visible";
+            document.getElementById("answerQ6").innerHTML = "No";
+            document.getElementById("after_submitQ6").style.background = "red";
+            document.getElementById("afterQ6_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+function checkQ7() {
+
+      if (document.question7.question.value == "c") {
+            document.getElementById("after_submitQ7").style.visibility = "visible";
+            document.getElementById("answerQ7").innerHTML = "Yes!! <br><br>In Anton syndrome there is blindness without awareness of the deficit. When the patient is asked about what he or she sees, the patient confabulates an answer.<br><br>The lesion in Anton syndrome is bilateral occipital. It can be argued that this lesion is not of higher cortical processing because the lesion is in the primary visual cortex. However, it can also be argued that it is a release phenomenon of the non-injured association cortex.";
+            document.getElementById("after_submitQ7").style.background = "lightgreen";
+            document.getElementById("afterQ7_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ7").style.visibility = "visible";
+            document.getElementById("answerQ7").innerHTML = "No";
+            document.getElementById("after_submitQ7").style.background = "red";
+            document.getElementById("afterQ7_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+function checkQ8() {
+
+      if (document.question8.question.value == "b") {
+            document.getElementById("after_submitQ8").style.visibility = "visible";
+            document.getElementById("answerQ8").innerHTML = "Yes!! <br><br>In Anton syndrome there is blindness without awareness of the deficit. When the patient is asked about what he or she sees, the patient confabulates an answer.<br><br>The lesion in Anton syndrome is bilateral occipital. It can be argued that this lesion is not of higher cortical processing because the lesion is in the primary visual cortex. However, it can also be argued that it is a release phenomenon of the non-injured association cortex.";
+            document.getElementById("after_submitQ8").style.background = "lightgreen";
+            document.getElementById("afterQ8_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ8").style.visibility = "visible";
+            document.getElementById("answerQ8").innerHTML = "No";
+            document.getElementById("after_submitQ8").style.background = "red";
+            document.getElementById("afterQ8_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+function checkQ9() {
+
+      if (document.question9.question.value == "i") {
+            document.getElementById("after_submitQ9").style.visibility = "visible";
+            document.getElementById("answerQ9").innerHTML = "Correct!! <br><br>The patient is Fyodor Dostoevsky. His very detailed descriptions of his seizure semiology through his characters allow to <a href='https://pubmed.ncbi.nlm.nih.gov/20561824/' target='_blank'>characterize his epilepsy in detail</a>.<br><br>The first episode is an <a href='https://pubmed.ncbi.nlm.nih.gov/26924970/' target='_blank'>ecstatic seizure, which is localized to the mesial temporal and insular regions</a>. <a href='https://pubmed.ncbi.nlm.nih.gov/26351599/' target='_blank'>A good case report of hyperreligiosity in epilepsy is presented here</a>.<br><br>The episode of seeing himself is called autoscopy, the hallucinatory experience of seeing one's own body in the external visual space. Not to be confused with an out-of-body experience in which one feels leaving the body and viewing it from another location outside. <a href='https://pubmed.ncbi.nlm.nih.gov/20561824/' target='_blank'>Autoscopy is seen, among other conditions, in temporal lobe epilepsy</a>.";
+            document.getElementById("after_submitQ9").style.background = "lightgreen";
+            document.getElementById("afterQ9_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ9").style.visibility = "visible";
+            document.getElementById("answerQ9").innerHTML = "No";
+            document.getElementById("after_submitQ9").style.background = "red";
+            document.getElementById("afterQ9_picture").innerHTML = "Try again!";
+
+}
+
+}
+
