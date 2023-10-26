0da33fac85177e23c2b1a9de10443156b546c056
IvanSanchezFernandez <31513069+IvanSanchezFernandez@users.noreply.github.com>
2023-10-25 22:40:29 -0400

    update

---
+++ Neurotoxicology_main.js
@@ -0,0 +1,128 @@
+
+
+function checkQ1() {
+
+      if (document.question1.question.value == "g") {
+            document.getElementById("after_submitQ1").style.visibility = "visible";
+            document.getElementById("answerQ1").innerHTML = "Correct!!<br><br>The appearance of yellow halos around lights, called xanthopsia, and altered color vision called chromatopsia are typical digoxin toxicities.<br><br>Digoxin is derived from the foxglove plant (digitalis species). It has inotropic effects and digitalis is used in the management of systolic dysfunction in patients with congestive heart failure and as an atrioventricular nodal blocking agent for managing atrial tachydysrhythmias. The plant was used initially for dropsy (edema).<br><br>Its narrow therapeutic index makes intoxication not unusual: 1% on digoxin develop toxicity. In pediatrics, 80% of digoxin intoxication arise from plant exposure and 20% through medication exposure.<br><br>The mechanism of action is through disruption of the Na-K ATPase with increased intracellular sodium which leads to increased intracellular calcium (decreased Na-Ca exchanger).<br><br>The most common symptoms are: gastrointestinal upset (nausea, vomiting, abdominal pain), bradycardia, palpitations, dyspnea, and syncope.<br><br>Management: EKG (premature ventricular contractions and bidirectional ventricular tachycardia), BMP, digoxin levels and repeat every 6 hours. There is a digoxin-specific antibody antigen-binding fragments, indicated for life-threatening toxicity: ventricular arrhythmias, high-grade heart blocks, hypotension, symptomatic bradycardia, K>5 meq/l, acute ingestion >10 mg (adult) or >4 mg (child), digoxin concentration >15 ng/ml or >10 ng/ml 6 hours post-ingestion.<br><br>More information <a href='https://www.ncbi.nlm.nih.gov/books/NBK470568/' target='_blank'>in this article</a> and <a href='https://www.youtube-nocookie.com/embed/m7MdhzMq-N4?playlist=m7MdhzMq-N4&autoplay=1&iv_load_policy=3&loop=1&modestbranding=1&start=' target='_blank'>in this video</a>.";
+            document.getElementById("after_submitQ1").style.background = "lightgreen";
+            document.getElementById("afterQ1_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ1").style.visibility = "visible";
+            document.getElementById("answerQ1").innerHTML = "No.";
+            document.getElementById("after_submitQ1").style.background = "red";
+            document.getElementById("afterQ1_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+function checkQ2() {
+
+      if (document.question2.question.value == "d") {
+            document.getElementById("after_submitQ2").style.visibility = "visible";
+            document.getElementById("answerQ2").innerHTML = "Exactly!!<br><br>Phencyclidine, a synthetic arycycloalkylamine created from piperidine and cyclohexanone, is ingested orally, injected intravenously, inhaled, or smoked. Inhalation (the most common route of administration) and intravenous routes of administration produce symptoms in 2 to 5 minutes. Oral ingestion produces symptoms in 30 to 60 minutes. The half-life is estimated to be 21 hours, but symptoms can last from several hours up to 48 hours, depending on the dose. Recurrent, fluctuating symptoms can occur because phencyclidine is fat-soluble and can be released from lipid stores and adipose tissue that can occur days to months after the initial use.<br><br>Phencyclidine is a non-competitive antagonist of the NMDA receptor which causes analgesia, anesthesia, cognitive defects, and psychosis. PCP blocks the uptake of dopamine and norepinephrine, leading to sympathomimetic effects such as hypertension, tachycardia, bronchodilation, and agitation. Phencyclidine can also cause sedation, muscarinic, and nicotinic signs by binding to acetylcholine receptors and GABA receptors. Sigma receptor stimulation by PCP causes lethargy and coma. It can cause nystagmus (horizontal, vertical, and rotary), confusion, hallucinations, delusions, violent behavior, muscle rigidity, rhabdomyolysis, and seizures.<br><br>Management: Supportive measures with particular care of cardiovascular stability and monitoring for rhabdomyolysis, non-stimulating environment, benzodiazepines.<br><br>Interestingly, it was initially used as an anesthetic until 1967.<br><br><a href='https://www.ncbi.nlm.nih.gov/books/NBK507865/' target='_blank'>More information in this article</a>.";
+            document.getElementById("after_submitQ2").style.background = "lightgreen";
+            document.getElementById("afterQ2_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ2").style.visibility = "visible";
+            document.getElementById("answerQ2").innerHTML = "No.";
+            document.getElementById("after_submitQ2").style.background = "red";
+            document.getElementById("afterQ2_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+function checkQ3() {
+
+      if (document.question3.question.value == "f") {
+            document.getElementById("after_submitQ3").style.visibility = "visible";
+            document.getElementById("answerQ3").innerHTML = "Correct!! <br><br>Nerve agents are a subcategory of organophosphorus compounds. They are used extensively as insecticides. The high toxicity profile, ease of synthesis, and widespread availability of these agents have lead to their use in chemical warfare over the years. The organophosphorus compounds ( OPCs) cross the dermis's epithelial membrane and the respiratory tract with ease. They then undergo biotransformation into their active form. They bind to the acetylcholine esterase and form an complex, which inhibits the hydrolysis of acetylcholine, causing it to accumulate and leads to a cholinergic crisis. The removal of the functional group in nerve agents makes it more deadly, as the bond between the agent and the enzyme becomes permanent.<br><br>The clinical presentation is usually hypercholinergic symptoms. The lungs and eyes absorb nerve agents the fastest. The presentation may include bronchorrhoea, chest tightness, loss of bladder control, salivation, vomiting, sweating, abdominal pain, and cramps. In severe cases, they may present with stupor, convulsions, and respiratory compromise.<br><br>Decontamination after donning appropriate personal protective equipment is the most important step. The first step is to disrobe the patient, rinse with soapy water, remove all jewelry and personal items and shift to the ICU if ventilatory support is required. Treatment includes atropine and supportive therapy (ventilatory support and anticonvulsants).<br><br><a href='https://www.ncbi.nlm.nih.gov/books/NBK570614/' target='_blank'>This article summarizes the most common bioterrorism agents</a>. <a href='https://www.yout-ube.com/watch?v=RTr1lquCQMg' target='_blank'>The Tokyo Sarin gas attack is an example of this</a>.";
+            document.getElementById("after_submitQ3").style.background = "lightgreen";
+            document.getElementById("afterQ3_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ3").style.visibility = "visible";
+            document.getElementById("answerQ3").innerHTML = "No";
+            document.getElementById("after_submitQ3").style.background = "red";
+            document.getElementById("afterQ3_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+function checkQ4() {
+
+      if (document.question4.question.value == "i") {
+            document.getElementById("after_submitQ4").style.visibility = "visible";
+            document.getElementById("answerQ4").innerHTML = "Yes!! <br><br>Botulinum neurotoxin is among the most toxic substances known to man in its purified form. Its extremely low lethal dose poses a grave biological threat, with high morbidity and mortality. The active form is made up of a C terminal heavy chain, which has the binding and translocation domains and a catalytic light chain forming the N terminal. The light chain has proteolytic activity and is considered the active part. BoNTs bind selectively and irreversibly to the nerve terminals and cleave SNARE (Soluble NSF Attachment Protein Receptor) proteins such as VAMP Synaptobrevin, Syntaxin, and SNAP-25. The SNARE proteins are responsible for the synaptic vesicle's fusion containing the neurotransmitter acutely choline with the presynaptic plasma membrane. This effectively blocks the release of acetylcholine, leading to flaccid paralysis.<br><br>Food-borne botulism is usually caused by ingestion of home-preserved food and presents typically within the first 4 hours. Infant botulism occurs by the ingestion of spores in infants one to six months in age and has an incubation period ranging from five to 30 days. Intestinal botulism is caused by the ingestion of spores by children over the age of years and adults and has a variable incubation period. Wound botulism is caused by the germination of spores in a wound and has an incubation period of 1 to 2 weeks.<br><br>Treatment is supportive care in the form of assisted ventilation, hydration, and prevention of secondary infections. Administration of the anti-toxin, preferably within the first 24 hours and not later than 72 hours, to neutralize the circulating BoNTs is a critical step in the management. In the EU, a trivalent (A, B, E; equine) antitoxin is used. In the United States in 2013, a human-derived BoNT antitoxin (BIG-IV) was approved by the FDA to treat infant botulism.<br><br>Botulinum neurotoxin is the deadliest toxin known to man. BoNTs are lethal at low doses (LD 50 is lower than any other known substance) and can be deployed through multiple routes, including aerosols or contaminated food and water. It is also colorless and odorless, making it ideal for silent attacks. <a href='https://www.ncbi.nlm.nih.gov/books/NBK570614/' target='_blank'>This article summarizes the most common bioterrorism agents</a>.";
+            document.getElementById("after_submitQ4").style.background = "lightgreen";
+            document.getElementById("afterQ4_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ4").style.visibility = "visible";
+            document.getElementById("answerQ4").innerHTML = "No";
+            document.getElementById("after_submitQ4").style.background = "red";
+            document.getElementById("afterQ4_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+function checkQ5() {
+
+      if (document.question5.question.value == "a") {
+            document.getElementById("after_submitQ5").style.visibility = "visible";
+            document.getElementById("answerQ5").innerHTML = "Correct!! <br><br>Although there might have been mass hysteria, the main potential toxic suspect was ergot poisoning.<br><br>Ergot is a fungus that can grow on rye and wheat. Ergot poisoning may include dizziness, seizures, and psychosis. Those proposing that this episode was due to ergot poisoning argue that the bread might have been contaminated causing a community food poisoning. Details are unclear.";
+            document.getElementById("after_submitQ5").style.background = "lightgreen";
+            document.getElementById("afterQ5_picture").innerHTML = "Great job!";
+
+}     else {
+            document.getElementById("after_submitQ5").style.visibility = "visible";
+            document.getElementById("answerQ5").innerHTML = "No";
+            document.getElementById("after_submitQ5").style.background = "red";
+            document.getElementById("afterQ5_picture").innerHTML = "Try again!";
+
+}
+
+}
+
+
+
+
+
+
+
